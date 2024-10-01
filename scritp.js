c19f282072b925ec19089edfc566713b5fbb9c93
palichocastro <104955968+palichocastro@users.noreply.github.com>
2024-10-01 16:11:23 -0300

    Create scritp.js

---
+++ scritp.js
@@ -0,0 +1,5 @@
+// Placeholder para alguna futura funcionalidad interactiva.
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Gracias por tu mensaje, me pondré en contacto contigo pronto.");
+});
